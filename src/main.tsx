32916fa2f3e7c296ba59882c97ae9116eb6dd616
Yogurt7v <egorov-2k@yandex.ru>
2024-06-02 18:39:47 +0300

    useQuery practice

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App';
 import './index.css';
+
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 5000,
+      retry: 1,
+      staleTime: 6000
+    }
+  }
+});
 
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 ).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <App />
+    <ReactQueryDevtools />
+    </QueryClientProvider>
   </React.StrictMode>
 );
--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,34 +1,41 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-
-interface Post {
-  id: number;
-  title: string;
-  body: string;
-  userId: number;
-}
+import usePosts from './hooks/usePosts';
+import { useState } from 'react';
 
 const PostList = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [error, setError] = useState('');
+  const pageSize = 12;
+  // const [page, setPage] = useState(1);
 
-  useEffect(() => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/posts')
-      .then((res) => setPosts(res.data))
-      .catch((error) => setError(error));
-  }, []);
+  const {data, error, isLoading, fetchNextPage} =  usePosts({ pageSize});
 
-  if (error) return <p>{error}</p>;
+  if(isLoading) return <p>Wait, please...</p>;
+
+  if (error) return <p>{error.message}</p>;
 
   return (
+    <>
+    {/* <select className='form-select mb-3' onChange={(e) => setUserId(parseInt(e.target.value)) } value={userId}>
+      <option value=""></option>
+      <option value="1">User1</option>
+      <option value="2">User2</option>
+      <option value="3">User3</option>
+    </select> */}
     <ul className="list-group">
-      {posts.map((post) => (
-        <li key={post.id} className="list-group-item">
-          {post.title}
-        </li>
+      {data.pages.map((page) => (
+        <>
+        {page.map((post) => (
+          <li key={post.id} className="list-group-item" >
+            {post.body}
+          </li>
+        )) }
+        </>
       ))}
+
     </ul>
+
+    <button className="btn btn-primary my-2" onClick={() => fetchNextPage()}>
+      Load
+    </button> 
+    </>
   );
 };
 
